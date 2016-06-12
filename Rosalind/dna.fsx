a172a8f947299745cf147bc7edf68b0f08d22935
Marcell Legeza <legezam@gmail.com>
2016-06-12 19:59:36 +0200

    added "dna" "rna"

---
+++ Rosalind/dna.fsx
@@ -0,0 +1,16 @@
+﻿#load "StringHelpers.fsx"
+#load "IO.fsx"
+
+let exercise = "dna"
+
+open System
+open System.Linq
+open IO
+let result = IO.ReadInput exercise
+                |> Seq.filter (fun char -> char <> '\n')
+                |> Seq.groupBy (fun item -> item)
+                |> Seq.sortBy (fun (key, value) -> key)
+                |> Seq.map (fun (_, value) -> value.Count())
+                |> (fun items -> System.String.Join(" ", items))
+
+IO.WriteOutput exercise result
---
+++ Rosalind/rna.fsx
@@ -0,0 +1,15 @@
+﻿#load "StringHelpers.fsx"
+#load "IO.fsx"
+
+let exercise = "rna"
+
+open System
+open System.Linq
+open IO
+
+let result = IO.ReadInput exercise
+                |> Seq.map (fun item -> if item = 'T' then 'U' else item)
+                |> (fun chars -> String.Join("", chars))
+
+
+IO.WriteOutput exercise result
